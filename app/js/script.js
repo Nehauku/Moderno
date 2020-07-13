42e3abe8625b3a79b64946d62db7960ff3b6dd0a
Nehauku <dimamichsnov@gmail.com>
2020-07-13 17:26:48 +0300

    added products-slider and stars rating

--- app/js/script.js
+++ app/js/script.js
@@ -1,7 +1,21 @@
 $(function () {
+	let mixer = mixitup('.products__inner-cards');
 
 
+	$('.product-slider__wrapper').slick({
+		infinite: true,
+		slidesToShow: 4,
+		slidesToScroll: 4,
+		dots: true,
+		arrows: false
+	});
 
+	$(".rate-star").rateYo({
+		rating: 5,
+		starWidth: "13px"
+	});
 
 
 });
+
+
