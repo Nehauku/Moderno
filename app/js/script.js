c2d77fc7280285c0c32b1d4f48b13b2d578deea3
Nehauku <dimamichsnov@gmail.com>
2020-07-22 09:31:42 +0300

    add modal end section product-one

--- app/js/script.js
+++ app/js/script.js
@@ -44,6 +44,15 @@ $(function () {
 		$('body').toggleClass('block');
 	});
 
+	$('.product-one__content-tabs .tab').on('click', function (event) {
+		var id = $(this).attr('data-id');
+		$('.product-one__content-tabs').find('.tab-item').removeClass('active-tab').hide();
+		$('.product-one__content-tabs .tabs').find('.tab').removeClass('active');
+		$(this).addClass('active');
+		$('#' + id).addClass('active-tab').fadeIn();
+		return false;
+	});
+
 
 
 });
